0c0f1ffd66527a4820ad4be033dc00629a0ed330
Johan Ehrenfors <johanehrenfors@hotmail.com>
2022-11-29 21:43:54 +0100

    Apply linter

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,10 @@
 import { Validator } from "./validator/validator";
 import { Rule } from "./validatorRules/rule";
 
-let ruleMatchesJohan: Rule = new Rule('Johan-matcher', (input: string) => { return (input === 'Johan'); });
-let testInputPositive = 'Johan';
-let testInputNegative = 'JOHAN';
-let validator = new Validator([ruleMatchesJohan]);
+const ruleMatchesJohan: Rule = new Rule('Johan-matcher', (input: string) => { return (input === 'Johan'); });
+const testInputPositive = 'Johan';
+const testInputNegative = 'JOHAN';
+const validator = new Validator([ruleMatchesJohan]);
 
 console.log(`Validating whether ${testInputPositive} matches the rule for "${ruleMatchesJohan.name}": ${validator.validate(testInputPositive)}`);
 console.log(`Validating whether ${testInputNegative} matches the rule for "${ruleMatchesJohan.name}": ${validator.validate(testInputNegative)}`);
--- tests/validator.test.ts
+++ tests/validator.test.ts
@@ -1,7 +1,7 @@
 import { Validator } from '../src/validator/validator';
 import { Rule } from '../src/validatorRules/rule';
  
-let testLengthRule = new Rule(
+const testLengthRule = new Rule(
     "String length has to be 10",
     (input: string) => { return input.length === 10; }
 );
@@ -9,28 +9,27 @@ let testLengthRule = new Rule(
 describe('Testing Validator class', () => {
   
   test('Constructor with no rules supplied (empty list)', () => {
-    let testValidator = new Validator([]);
+    const testValidator = new Validator([]);
     expect(testValidator.rules.length).toBe(0);
   });
 
   test('Constructor with one rule has one rule', () => {
-    let testRule = new Rule("test", (input: string) => { return true; })
-    let testValidator = new Validator([testRule]);
+    const testValidator = new Validator([testLengthRule]);
     expect(testValidator.rules.length).toBe(1);
   });
 
   test('Validating without any rules returns true', () => {
-    let testValidator = new Validator([]);
+    const testValidator = new Validator([]);
     expect(testValidator.validate("some string to validate")).toBe(true);
   });
 
   test('Validating with a broken rule returns false', () => {
-    let testValidator = new Validator([testLengthRule]);
+    const testValidator = new Validator([testLengthRule]);
     expect(testValidator.validate("too short")).toBe(false);
   });
 
   test('Validating with an adhered rule returns true', () => {
-    let testValidator = new Validator([testLengthRule]);
+    const testValidator = new Validator([testLengthRule]);
     expect(testValidator.validate("just right")).toBe(true);
   });
 
